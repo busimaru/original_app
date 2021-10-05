9ba7a072c0d1683b8ed7f228f72eea67d4ca0a8e
busimaru <ntdexam@icloud.com>
2021-10-05 15:35:00 +0900

    Revert "トップページにgoogle map表示"
    This reverts commit 21c5e4c276a0c97c36c87a118619f1e5e7f36c44.

--- app/controllers/topviews_controller.rb
+++
@@ -1,4 +0,0 @@
-class TopviewsController < ApplicationController
-  def index
-  end
-end
--- db/migrate/20211002044727_create_topviews.rb
+++
@@ -1,8 +0,0 @@
-class CreateTopviews < ActiveRecord::Migration[6.0]
-  def change
-    create_table :topviews do |t|
-
-      t.timestamps
-    end
-  end
-end
